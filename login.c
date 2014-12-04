8a644bdfc1de69468aedd258b61343110a4ee645
Tong YI <tyi@open-22.CS.McGill.CA>
2014-12-04 13:22:34 -0500

    standalone login

--- login.c
+++ login.c
@@ -2,35 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char* getfield(char* line, int num)
-{
-	const char* tok;
-	for (tok = strtok(line, ",");
-		tok && *tok;
-		tok = strtok(NULL, ",\n"))
-	{
-		if (!--num)
-			return tok;
-	}
-	return NULL;
-}
-
 int main(int argc, char *argv[])
-{
-	FILE* stream = fopen("./Members.csv", "r");
-	if(argc>1)
-	{
-		char line[1024];
-		while (fgets(line, 1024, stream))
-		{
-			char* tmp = strdup(line);
-			//printf("Field 3 would be %s\n", getfield(tmp, 3));
-			// NOTE strtok clobbers tmp
-			if(strcmp(argv[1], getfield(tmp,3))==0)
-				printf("hi");
-			free(tmp);
-		}
-	}
+{	
+   FILE* membersData = fopen("./Members.csv", "r");
+   FILE* loggedIn = fopen("./LoggedIn.csv", "w");
+   char tempLine[1024];
+   if(argc==3)
+   {
+      int successLogin = 1;
+      char* tempName;
+      char* tempUsername;
+      char* tempPass;
+      while (fgets(tempLine, 1024, membersData))
+      {
+	 tempName = strtok(tempLine, ",");
+         tempUsername = strtok(NULL, ",");
+         tempPass = strtok(NULL,"\n");
+         if((strcmp(argv[1],tempUsername)==0)&&(strcmp(argv[2],tempPass)==0))
+         {
+            fprintf(loggedIn, "%s\n", argv[1]);
+            successLogin = 0;
+            //go to catalouge
+         }
+         memset(tempLine,0,sizeof(tempLine));
+      }
+      if(successLogin != 0)
+      {
+         //go to error page
+         printf("Wrong username and/or password\n");
+      }
+      
+   }
+   else
+   {
+      printf("Missing Input\n");
+   }
 }
-
-
