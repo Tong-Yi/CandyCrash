92e7c3725e0f3bc3322bb4f0351e7c22d417eac5
Tong YI <tyi@open-19.CS.McGill.CA>
2014-12-05 16:03:52 -0500

    login updated with cgi post

--- login.c
+++ login.c
@@ -1,42 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-int main(int argc, char *argv[])
+void errorMessage()
+{
+   printf("%s%c%c\n","Content-Type:text/html",13,10);
+   printf("%s\n\n","Content-Type:text/html");
+   printf("Content-Type:text/html\n\n");
+   printf("<html>");
+   printf("<head><title>ERROR</title></head>");
+   printf("<body><p>Wrong username and/or password!</p><body>");
+   printf("</html>");
+}
+int main()
 {	
+   char inputedUsername[200];
+   char inputedPassword[200];
+   char c;
+   int a = 0;
+   int b = 0;
+   int lengthCounter = 0;
+   
+   int n = atoi(getenv("CONTENT_LENGTH"));
+   int firstWord = 0;
+
    FILE* membersData = fopen("./members.csv", "r");
    FILE* loggedIn = fopen("./loggedin.csv", "w");
    char tempLine[1024];
-   if(argc==3)
+   int successLogin = 1;
+   char* tempName;
+   char* tempUsername;
+   char* tempPass;
+   
+   while((c = getchar()) != EOF && lengthCounter<n && a<200 && b<200)
    {
-      int successLogin = 1;
-      char* tempName;
-      char* tempUsername;
-      char* tempPass;
-      while (fgets(tempLine, 1024, membersData))
+      if(firstWord = 0)
       {
-	 tempName = strtok(tempLine, ",");
-         tempUsername = strtok(NULL, ",");
-         tempPass = strtok(NULL,"\n");
-         if((strcmp(argv[1],tempUsername)==0)&&(strcmp(argv[2],tempPass)==0))
+         if(c!='+')
          {
-            fprintf(loggedIn, "%s\n", argv[1]);
-            successLogin = 0;
-            //go to catalouge
+            inputedUsername[a] = c;
+            a++;
          }
-         memset(tempLine,0,sizeof(tempLine));
+         else firstWord = 1;
       }
-      if(successLogin != 0)
+      else
       {
-         //go to error page
-         printf("Wrong username and/or password\n");
+         inputedPassword[b] = c;
+         b++;
       }
-      
+      lengthCounter++;
    }
-   else
+
+
+   while (fgets(tempLine, 1024, membersData))
    {
-      printf("Missing Input\n");
+      tempName = strtok(tempLine, ",");
+      tempUsername = strtok(NULL, ",");
+      tempPass = strtok(NULL,"\n");
+      if((strcmp(argv[1],tempUsername)==0)&&(strcmp(argv[2],tempPass)==0))
+      {
+         fprintf(loggedIn, "%s\n", argv[1]);
+         successLogin = 0;
+         //go to catalouge
+      }
+      memset(tempLine,0,sizeof(tempLine));
    }
+   if(successLogin != 0)
+   {
+      //go to error page
+      errorMessage();
+   }
+
 }
 
 
